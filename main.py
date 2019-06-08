bfed0ff001576cbef3e6976015a226ddfba659e8
Fabio Chicout <fchicout@gmail.com>
2019-06-07 21:22:10 -0300

    more little improvements on main

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
 
 import sys
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
 
 
-def main(args):
-    print(args)
+def main():
+    r = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            print('Falaê: ')
+            audio = r.listen(source)
+            try:
+                text = r.recognize_google(audio, language='pt-BR')
+                print('You said: {}'.format(text))
+                if format(text) == 'L1 na escuta':
+                    speech=gTTS("i'm fine dude")
+                    speech.save('response.mp3')
+                    playsound('response.mp3')
+                elif format(text) == 'parou':
+                    break
+                os.remove('response.mp3')
+            except:
+                print('Sorry, cant recognize')
+
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
--- runners/TalkRunner.py
+++ runners/TalkRunner.py
@@ -4,7 +4,7 @@ from Runner import Runner
 
 class TalkRunner(Runner):
     def __init___(self, message2Talk):
-        speech=gTTS("i'm fine")
+        speech=gTTS(message2Talk)
         speech.save('response.mp3')
         playsound('response.mp3')
         os.remove('response.mp3')
