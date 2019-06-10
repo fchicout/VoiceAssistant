87048b5853a56085f32d51c0307530e72043d4b5
Fabio Chicout <fchicout@gmail.com>
2019-06-10 18:59:13 -0300

    adicionando redução de ruido na captação do microfone

--- main.py
+++ main.py
@@ -10,21 +10,29 @@ def main():
     while True:
         with sr.Microphone() as source:
             print('Falaê: ')
+            r.adjust_for_ambient_noise(source, duration=0.5)
             audio = r.listen(source)
             try:
                 text = r.recognize_google(audio, language='pt-BR')
-                print('You said: {}'.format(text))
+                print('You greeted: {}'.format(text))
                 if format(text) == 'L1 na escuta':
-                    greet = TalkRunner("i'm fine dude")
+                    greet = TalkRunner("i'm fine dude, what do you want")
                     greet.run()
                     while True:
-                        pass
+                        audio = r.listen(source)
+                        try:
+                            command = r.recognize_google(audio, language='pt-BR')
+                            # Work out actions here
+                            print('Your command: {}'.format(command))
+                            if format(text) == 'pausa':
+                                break
+                        except:
+                            pass
                 elif format(text) == 'parou':
                     break
                 os.remove('response.mp3')
             except:
                 print('Sorry, cant recognize')
-
 
 
 if __name__ == '__main__':
